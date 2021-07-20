8dad4477820e95919b76268354c03a40c864ebb5
Balu Praveen Datty <dev.badatt@gmail.com>
2021-07-20 16:48:15 +0530

    Tailwind with components

--- client/src/app/containers/Home/Home.tsx
+++ client/src/app/containers/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RouteComponentProps } from 'react-router';
 import 'app/styles/global.css';
+import * as Styled from './styles';
 
 export namespace HomeProps {
   export interface Props extends RouteComponentProps<void> {}
@@ -9,7 +10,7 @@ export namespace HomeProps {
 export const Home = ({ history, location }: HomeProps.Props) => {
   return (
     <div>
-      <div className="text-blue-400">This is the entry point for the application</div>
+      <Styled.H1>This is the entry point for the application</Styled.H1>
     </div>
   );
 };
---
+++ client/src/app/containers/Home/styles.ts
@@ -0,0 +1,10 @@
+import tw from 'tailwind-styled-components';
+
+export const H1 = tw.h1`
+    flex
+    items-center
+    justify-center
+    flex-col
+    w-full
+    p-3
+`;
