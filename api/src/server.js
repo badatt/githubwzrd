a63936c4d8a30781adf57a70b2d35bc411a0203f
Balu Praveen Datty <dev.badatt@gmail.com>
2021-08-14 14:19:06 +0530

    Code updated

--- api/src/server.js
+++ api/src/server.js
@@ -1,4 +1,3 @@
-// make bluebird default Promise
 Promise = require('bluebird'); // eslint-disable-line no-global-assign
 const { port, env } = require('./config/vars');
 const logger = require('./config/logger');
