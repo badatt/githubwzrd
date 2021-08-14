814b0bd47b71e07e6f8e8a7d786c0c56a9d2279f
Balu Praveen Datty <dev.badatt@gmail.com>
2021-08-14 14:11:42 +0530

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -9,14 +9,6 @@ const { getSecretValue } = require('./config/aws');
 const handler = serverless(app);
 
 module.exports.handler = async (event, context) => {
-  // you can do other things here
-
-  //const cryptoKeys = await getSecretValue(process.env.CRYPTO_KEYS_SECRET_NAME);
-  //process.env['JWT_SECRET'] = cryptoKeys.PUBLIC_KEY;
-
   logger.info(JSON.stringify(event));
-  const result = await handler(event, context);
-  // and here
-  logger.info(JSON.stringify(result));
-  return result;
+  return await handler(event, context);
 };
