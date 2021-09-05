21bef77a8598095c483b7a479a227c820fabec6a
Balu Praveen Datty <dev.badatt@gmail.com>
2021-09-06 03:20:08 +0530

    Removed footer text

--- client/src/containers/Footer/Footer.container.tsx
+++ client/src/containers/Footer/Footer.container.tsx
@@ -6,24 +6,7 @@ export default () => {
     <Fragment>
       <footer className={cl.main}>
         <hr className={cl.separator} />
-        <main className={cl.footer}>
-          <iframe
-            frameBorder="0"
-            height="20px"
-            scrolling="0"
-            src="https://ghbtns.com/github-btn.html?user=badatt&repo=githubwzrd&type=star&count=true"
-            title="GitHub Stars"
-            width="110px"
-          />
-          <iframe
-            frameBorder="0"
-            height="20px"
-            scrolling="0"
-            src="https://ghbtns.com/github-btn.html?user=badatt&type=follow&count=true"
-            title="GitHub Follow"
-            width="130px"
-          />
-        </main>
+        <main className={cl.footer}></main>
       </footer>
     </Fragment>
   );
