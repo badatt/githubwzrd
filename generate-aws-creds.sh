fc21e03e5e4988c2f81a7736fabb6a22213e415f
badatt <dev.badatt@gmail.com>
2021-10-09 02:22:19 +0530

    shell script changes

--- generate-aws-creds.sh
+++ generate-aws-creds.sh
@@ -1,14 +1,12 @@
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
-unset AWS_SESSION_TOKEN
+ #!/bin/sh
 
 ENV=$1
 
-if [ ${ENV} == "sbx" ]
+if [ "$ENV" = "sbx" ]
 then
     TARGET_ROLE="arn:aws:iam::965776723730:role/DefaultAccountAccessRole"
     PROFILE="ct-sbx"
-elif [ ${ENV} == "prd" ]
+elif [ "$ENV" = "prd" ]
 then
     TARGET_ROLE="arn:aws:iam::904828318817:role/DefaultAccountAccessRole"
     PROFILE="badatt-dev-prd"
@@ -16,19 +14,22 @@ fi
 
 echo "Assuming IAM role ${TARGET_ROLE}"
 
-assumeRoleResponse=($(
+TEMP_ROLE=$(
 aws sts assume-role --role-arn ${TARGET_ROLE} \
-    --role-session-name "session-$(Guidgen)" --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
-    --output text \
+    --role-session-name "session-$(uuidgen)" \
     --profile ${PROFILE}
-))
+)
 
-export AWS_ACCESS_KEY_ID=${assumeRoleResponse[0]}
-export AWS_SECRET_ACCESS_KEY=${assumeRoleResponse[1]}
-export AWS_SESSION_TOKEN=${assumeRoleResponse[2]}
+AWS_ACCESS_KEY_ID=$(echo ${TEMP_ROLE} | jq -r '.Credentials.AccessKeyId')
+AWS_SECRET_ACCESS_KEY=$(echo ${TEMP_ROLE} | jq -r '.Credentials.SecretAccessKey')
+AWS_SESSION_TOKEN=$(echo ${TEMP_ROLE} | jq -r '.Credentials.SessionToken')
+
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
 
 cat << EOF >  .aws.env
-AWS_ACCESS_KEY_ID=${assumeRoleResponse[0]}
-AWS_SECRET_ACCESS_KEY=${assumeRoleResponse[1]}
-AWS_SESSION_TOKEN=${assumeRoleResponse[2]}
+AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
 EOF
