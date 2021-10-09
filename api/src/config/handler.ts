0811b5cb3877ac552115f564574f8df0b536331b
badatt <dev.badatt@gmail.com>
2021-10-09 13:54:40 +0530

    api changes

--- api/src/config/handler.ts
+++ api/src/config/handler.ts
@@ -9,7 +9,7 @@ export function requestHandler(asyncFunction: any) {
       await asyncFunction(req, res, next);
     } catch (e: any) {
       logger.error(e);
-      return next(new APIError({ message: e.message, status: INTERNAL_SERVER_ERROR }));
+      return next(new APIError({ message: e.message, status: e.status || INTERNAL_SERVER_ERROR }));
     }
   };
 }
