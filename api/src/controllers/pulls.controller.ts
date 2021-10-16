a098550626774d0ccdf1915ba1ce187690643998
badatt <dev.badatt@gmail.com>
2021-10-16 22:15:23 +0530

    wip pulls

--- api/src/controllers/pulls.controller.ts
+++ api/src/controllers/pulls.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { NOT_FOUND } from 'http-status';
+import { NOT_FOUND, UNAUTHORIZED } from 'http-status';
 import { gh } from '../config/github-graphql-client';
 import { db } from '../config/aws';
 import APIError from '../errors/APIError';
@@ -104,4 +104,84 @@ export const relatedPulls = async (req: Request, res: Response, next: NextFuncti
   }
 
   res.send({ data: response, rateLimit: rl });
+};
+
+export const relatedPull = async (req: Request, res: Response, next: NextFunction) => {
+  const { repo } = req.params;
+  const { org, gitToken, userId, username } = req.currentUser;
+  const userItem = await db.tryGetAsync(new User(), { id: userId, org: org });
+  if (!userItem.repos || userItem.repos.length === 0)
+    throw new APIError({ message: 'No repos found', status: NOT_FOUND });
+  if (!userItem.repos.includes(repo))
+    throw new APIError({ message: 'Do not have access to the repo', status: UNAUTHORIZED });
+
+  // TODO change query to pull only OPEN state pulls
+  const queryTemplate = `
+    query relatedPulls($org: String!, $repo: String!) {
+      organization(login:$org) {
+        repository(name: $repo) {
+          name
+          url
+          pullRequests(first:100, orderBy: {field: CREATED_AT, direction: DESC} ) {
+            totalCount
+            nodes {
+              title
+              url
+              author{
+                login
+              }
+              reviewRequests(first:100) {
+                nodes{
+                  requestedReviewer{
+                  ... on Actor {
+                      login
+                    }
+                  }
+                }
+              }
+              assignees(first:100) {
+                nodes {
+                  login
+                }
+              }
+            }
+          }
+        }
+      }
+      rateLimit {
+        limit
+        cost
+        remaining
+        resetAt
+      }
+    }
+  `;
+
+  const {
+    organization: { repository },
+    rateLimit,
+  } = await gh(gitToken)(queryTemplate, { org, repo });
+  const relatedPull: IRelatedPull = {
+    repoName: repository.name,
+    repoUrl: repository.url,
+    pulls: [],
+  };
+  for (const p of repository.pullRequests.nodes) {
+    const pull: IPull = { title: p.title, url: p.url };
+    if (p.author.login === username) {
+      relatedPull.anyCreatedByMe = true;
+      pull.createdByMe = true;
+    }
+    if (p.reviewRequests.nodes.some((n: any) => n.requestedReviewer.login === username)) {
+      relatedPull.anyReviewRequiredByMe = true;
+      pull.reviewRequiredByMe = true;
+    }
+    if (p.assignees.nodes.some((n: any) => n.login === username)) {
+      relatedPull.anyAssignedToMe = true;
+      pull.assignedToMe = true;
+    }
+    relatedPull.pulls.push(pull);
+  }
+
+  res.send({ data: relatedPull, rateLimit });
 };
--- api/src/routes/pulls.route.ts
+++ api/src/routes/pulls.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { requestHandler } from '../config/handler';
 import { authorize } from '../middlewares/auth';
-import { relatedPulls } from '../controllers/pulls.controller';
+import { relatedPulls, relatedPull } from '../controllers/pulls.controller';
 
 const router = Router();
 
 router.route('/_meta').get((req, res) => res.send({ status: '👍' }));
 
 router.route('/').get(authorize(), requestHandler(relatedPulls));
+router.route('/:repo').get(authorize(), requestHandler(relatedPull));
 
 export default router;
